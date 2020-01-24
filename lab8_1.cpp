d0e7d0075e0c8a6f5bbf9af6f93c882a9b54cf45
PackYokohama <59277976+PackYokohama@users.noreply.github.com>
2020-01-24 17:04:10 +0700

    Update lab8_1.cpp

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -1,12 +1,18 @@
 #include<iostream>
-#include<iomanip> //For using setw(), setprecision(), ...
+#include<iomanip> 
 using namespace std;
 
+float pb,per,pm,inter,total,nb = 1;
+int n=0;
 int main(){	
+	cout << "Enter initial loan: ";
+	cin >> pb;
+	cout << "Enter interest rate per year (%): ";
+	cin >> per;
+	cout << "Enter amount you can pay per year: ";
+	cin >> pm;
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
+	
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -15,16 +21,26 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
 	
+	while (nb != 0){
+	n++;
+	cout << fixed << setprecision(2); 
+	cout << setw(13) << left << n;
+	cout << setw(13) << left << pb;
+	inter = (pb*per)/100;
+	cout << setw(13) << left << inter;
+	total = pb+inter;
+	cout << setw(13) << left << total;
+	if(pm <= total){
+	cout << setw(13) << left << pm ;
+}else{ 
+	pm=total;
+	cout << setw(13) << left << pm ;
+}
+	nb = total-pm;
+	cout << setw(13) << left << nb;
+	pb=nb;
+	cout << "\n";	
+	}
 	return 0;
 }
